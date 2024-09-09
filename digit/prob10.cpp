7a18ba8159a4c8116a1138a3f8576f190761309d
Fariz36 <72541852+Fariz36@users.noreply.github.com>
2024-09-09 20:55:21 +0700

    Add files via upload

---
+++ digit/prob10.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ii pair<int, int>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vii vector<ii>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define per(i, a, b) for (int i = a; i >= b; i--)
+#define endl '\n'
+#define sz(x) (int)x.size()
+
+const int mod = 1e9+7;
+
+void solve() {
+	//count how much time needed
+	long double phi = (1 + sqrt(5)) / 2;
+
+  int x = (long long)(6969696969 + 6969696870) * 50;
+
+	cout << (long long)(x*log10(phi) - 100*log10(sqrt(5))) + 1 << endl;
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int tc = 1;
+  //cin >> tc;
+  
+  while (tc--) {
+    solve();
+  }
+}
